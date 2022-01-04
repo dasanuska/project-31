ea08392cb85681fb5233e3f0b30060d3285c6de3
dasanuska <92671275+dasanuska@users.noreply.github.com>
2022-01-04 12:00:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -75,26 +75,27 @@ function draw() {
   playerArcher.display();
   computerArcher.display()
 
- // Uncomment and use correct for loop to display arrow using showArrow() function
- for (var i=0; i<playerArrows.length; i++) 
- {
- showArrows(i, playerArrows);
- }
+ // Use for loop to display arrow using showArrow() function
+ for (var i = 0; i < playerArrows.length; i++) {
+  showArrows(i, playerArrows);
+}
 
 }
 
-
-
 function keyPressed() {
+
   if(keyCode === 32){
     // create an arrow object and add into an array ; set its angle same as angle of playerArcher
     var posX = playerArcher.body.position.x;
     var posY = playerArcher.body.position.y;
     var angle = playerArcher.body.angle+PI/2;
+
     var arrow = new PlayerArrow(posX, posY, 100, 10);
+
     arrow.trajectory = [];
     Matter.Body.setAngle(arrow.body, angle);
     playerArrows.push(arrow);
+
   }
 }
 
